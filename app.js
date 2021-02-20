58c6b9e8432ec1346fe65fb8c3899a6eebc998ca
adamesalles <adamesalles111@gmail.com>
2021-02-20 19:55:23 -0300

    Corrigindo b=0

--- app.js
+++ app.js
@@ -17,14 +17,18 @@ btn.onclick = () => {
     Delta: math.sqrt(delta)
   };
   if (aC == 0){
-    eqShow.innerText = `$$ ${bC != 1 ? bC : ''}x + ${cC} = 0 $$ $$ x = \\dfrac{-${bC}}{${cC}} $$`
-    let ans = math.evaluate('-C/B', scope);
-    showAnswer('A raíz é: \n' + `$x = ${ans}$`)
+    if (bC == 0) {
+      eqShow.innerText = `$$ ${cC} = 0 $$`;
+      ( (cC == 0) ? showAnswer('A raíz é: \n $x = 0$'): showAnswer('A equação não é verdadeira, portanto não há raíz(es).'));
+    } else {
+      eqShow.innerText = `$$ ${bC != 1 ? bC : ''}x + ${cC} = 0 $$ $$ x = \\dfrac{-${cC}}{${bC}} $$`;
+      let ans = math.evaluate('-C/B', scope);
+      showAnswer('A raíz é: \n' + `$x = ${ans}$`);
+    }
   }else {
     eqShow.innerText = `$$ ${aC != 1 ? aC : ''} x^2 + ${bC != 1 ? bC : ''}x + ${cC} = 0 $$ $$ \\Delta = ${bC}^2 - 4 \\cdot ${aC} \\cdot ${cC} = ${delta}  $$ $$ x = \\dfrac{-${bC} \\pm \\sqrt{${delta}}}{2\\cdot ${aC}} = \\dfrac{-${bC} \\pm ${math.sqrt(delta)}}{${2 * aC}}$$`
     let ans = math.evaluate(['(-B + Delta)/(2*A)','(-B - Delta)/(2*A)'], scope);
     showAnswer('As raízes são: \n' + ((ans[0].im != undefined) ? `$x_1 = ${ans[0].re} + ${ans[0].im}i$ \n $ x_2 = ${ans[1].re} + ${ans[1].im}i$` : `$ x_1 = ${ans[0]}$ \n $ x_2 = ${ans[1]}$`));
-    console.log(ans[0].im)
   }};
 
 function showAnswer(ans){
